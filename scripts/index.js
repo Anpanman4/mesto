c45494abe49b75de5fd6de23911cff5c636557a2
Aleksey Kondratev <Anpanman4@yandex.ru>
2022-10-20 19:14:53 +0300

    fix: do fix

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,37 @@ const profileElement = document.querySelector('.profile');
 const popupEditButtonElement = profileElement.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-const popupSaveButtonElement = popupElement.querySelector('.popup__save-button');
+const popupForm = popupElement.querySelector('.popup__form');
 
+// ф-я для открытия popup
 const addPopupVisibility = function() {
-	let profileName = profileElement.querySelector('.profile__name').textContent;
-	let profileJob = profileElement.querySelector('.profile__job').textContent;
-	popupElement.querySelector('.popup__field-name').value = profileName;
-	popupElement.querySelector('.popup__field-job').value = profileJob;
+	popupElement.querySelector('.popup__field_type_name').value = profileElement.querySelector('.profile__name').textContent;
+	popupElement.querySelector('.popup__field_type_job').value = profileElement.querySelector('.profile__job').textContent;
 	popupElement.classList.add('popup_opened');
 }
 
+// ф-я для закрытия popup
 const removePopupVisibility = function() {
 	popupElement.classList.remove('popup_opened');
 }
 
+// ф-я для сохранения новых значений popup
 const savePopupValue = function(event) {
 	event.preventDefault()
-	let profileNameValue = popupElement.querySelector('.popup__field-name').value;
-	let profileJobValue = popupElement.querySelector('.popup__field-job').value;
-	profileElement.querySelector('.profile__name').textContent = profileNameValue;
-	profileElement.querySelector('.profile__job').textContent = profileJobValue;
-	popupElement.classList.remove('popup_opened');
-}
-
-const closePopupOnClickOverlay = function(event) {
-	if (event.target !== event.currentTarget) {
-		return;
-	}
+	profileElement.querySelector('.profile__name').textContent = popupElement.querySelector('.popup__field_type_name').value;
+	profileElement.querySelector('.profile__job').textContent = popupElement.querySelector('.popup__field_type_job').value;
 	removePopupVisibility();
 }
 
+// ф-я для закрытия popup по клику вне формы
+// const closePopupOnClickOverlay = function(event) {
+// 	if (event.target !== event.currentTarget) {
+// 		return;
+// 	}
+// 	removePopupVisibility();
+// }
+
 popupEditButtonElement.addEventListener('click', addPopupVisibility);
 popupCloseButtonElement.addEventListener('click', removePopupVisibility);
-popupElement.addEventListener('click', closePopupOnClickOverlay);
-popupSaveButtonElement.addEventListener('click', savePopupValue);
+popupForm.addEventListener('submit', savePopupValue);
+// popupElement.addEventListener('click', closePopupOnClickOverlay);
