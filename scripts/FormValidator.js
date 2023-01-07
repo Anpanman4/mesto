f12b966d9d49195fca97cacb9fcd9ca3534578a3
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-01-07 17:14:50 +0300

    feat: do some refactor

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,64 +1,64 @@
 export default class FormValidator {
-	constructor(validationConfig, form) {
-		this._validationConfig = validationConfig;
-		this._form = form;
-		this._inputList = Array.from(this._form.querySelectorAll(this._validationConfig.inputSelector));
-		this._submitButton = this._form.querySelector(this._validationConfig.submitButtonSelector);
-	}
+  constructor(validationConfig, form) {
+    this._validationConfig = validationConfig;
+    this._form = form;
+    this._inputList = Array.from(this._form.querySelectorAll(this._validationConfig.inputSelector));
+    this._submitButton = this._form.querySelector(this._validationConfig.submitButtonSelector);
+  }
 
-	_showInputError = (errorMessage) => {
-		this._errorElement.textContent = errorMessage;
-		this._errorElement.classList.add(this._validationConfig.errorInputActiveClass);
-	}
+  _showInputError = (errorMessage) => {
+    this._errorElement.textContent = errorMessage;
+    this._errorElement.classList.add(this._validationConfig.errorInputActiveClass);
+  }
 
-	_hideInputError = () => {
-		this._errorElement.textContent = "";
-		this._errorElement.classList.remove(this._validationConfig.errorInputActiveClass);
-	}
+  _hideInputError = () => {
+    this._errorElement.textContent = "";
+    this._errorElement.classList.remove(this._validationConfig.errorInputActiveClass);
+  }
 
-	_toggleInputErrorState = (inputElement) => {
-		const isValid = inputElement.validity.valid;
+  _toggleInputErrorState = (inputElement) => {
+    const isValid = inputElement.validity.valid;
 
-		const formInputSection = inputElement.closest(this._validationConfig.sectionSelector);
-		this._errorElement = formInputSection.querySelector(this._validationConfig.errorInputSelector);
+    const formInputSection = inputElement.closest(this._validationConfig.sectionSelector);
+    this._errorElement = formInputSection.querySelector(this._validationConfig.errorInputSelector);
 
-		if (isValid) {
-			this._hideInputError();
-		} else {
-			this._showInputError(inputElement.validationMessage);
-		}
-	}
+    if (isValid) {
+      this._hideInputError();
+    } else {
+      this._showInputError(inputElement.validationMessage);
+    }
+  }
 
-	_turnOffButtom = () => {
-		this._submitButton.setAttribute("disabled", true);
-		this._submitButton.classList.add(this._validationConfig.inactiveButtonClass);
-	}
+  _turnOffButtom = () => {
+    this._submitButton.setAttribute("disabled", true);
+    this._submitButton.classList.add(this._validationConfig.inactiveButtonClass);
+  }
 
-	_turnOnButtom = () => {
-		this._submitButton.removeAttribute("disabled");
-		this._submitButton.classList.remove(this._validationConfig.inactiveButtonClass);
-	}
+  _turnOnButtom = () => {
+    this._submitButton.removeAttribute("disabled");
+    this._submitButton.classList.remove(this._validationConfig.inactiveButtonClass);
+  }
 
-	_toggleButtonState = () => {
-		const hasInvalidInput = this._inputList.some(inputElement => !inputElement.validity.valid);
+  _toggleButtonState = () => {
+    const hasInvalidInput = this._inputList.some(inputElement => !inputElement.validity.valid);
 
-		if (hasInvalidInput) {
-			this._turnOffButtom();
-		} else {
-			this._turnOnButtom();
-		}
-	}
+    if (hasInvalidInput) {
+      this._turnOffButtom();
+    } else {
+      this._turnOnButtom();
+    }
+  }
 
-	_setEventListeners = () => {
-		this._inputList.forEach((inputElement) => {
-			inputElement.addEventListener('input', () => {
-				this._toggleInputErrorState(inputElement);
-				this._toggleButtonState()
-			})
-		})
-	}
+  _setEventListeners = () => {
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._toggleInputErrorState(inputElement);
+        this._toggleButtonState()
+      })
+    })
+  }
 
-	enableValidation = () => {
-		this._setEventListeners();
-	}
+  enableValidation = () => {
+    this._setEventListeners();
+  }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -29,81 +29,81 @@ const textPopup = imagePopup.querySelector('.popup__text');
 
 // ф-я создания карточки с совсеми ивеннтами
 const makeCard = function(card) {
-	return new Card(card, '#element-template', openModalImage).render();
+  return new Card(card, '#element-template', openModalImage).render();
 }
 
 // ф-я для добавления карточки	
 const renderCard = function(card) {
-	elementContainer.prepend(makeCard(card));
+  elementContainer.prepend(makeCard(card));
 }
 
 // ф-я для открытия popup с его значениями
 const openEditModalWindow = function() {
-	popupEditFieldName.value = profileName.textContent;
-	popupEditFieldJob.value = profileJob.textContent;
-	openModalWindow(popupEditElement);
+  popupEditFieldName.value = profileName.textContent;
+  popupEditFieldJob.value = profileJob.textContent;
+  openModalWindow(popupEditElement);
 }
 
 // ф-я для открытия popup
 const openModalWindow = function(item) {
-	item.classList.add('popup_opened');
-	document.addEventListener('keydown', closeModalWindowByEsc)
+  item.classList.add('popup_opened');
+  document.addEventListener('keydown', closeModalWindowByEsc)
 }
 
 // ф-я для закрытия popup
 const closeModalWindow = function(item) {
-	item.classList.remove('popup_opened');
-	document.removeEventListener('keydown', closeModalWindowByEsc)
+  item.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closeModalWindowByEsc)
 }
 
 // ф-я для закрытия popup по нажатию на esc
 const closeModalWindowByEsc = function(evt) {
-	if (evt.key === "Escape") {
-		closeModalWindow(document.querySelector('.popup_opened'));
-	}
+  if (evt.key === "Escape") {
+    closeModalWindow(document.querySelector('.popup_opened'));
+  }
 }
 
 // ф-я для сохранения новых значений у edit popup
 const saveEditPopupValues = function(evt) {
-	evt.preventDefault()
-	profileName.textContent = popupEditFieldName.value;
-	profileJob.textContent = popupEditFieldJob.value;
-	closeModalWindow(popupEditElement);
+  evt.preventDefault()
+  profileName.textContent = popupEditFieldName.value;
+  profileJob.textContent = popupEditFieldJob.value;
+  closeModalWindow(popupEditElement);
 }
 
 // ф-я отвечающия за добавление новых карточек
 const addNewCard = function(evt) {
-	evt.preventDefault()
-	const valueCard = {
-		name: popupAddFieldName.value,
-		link: popupAddFieldImage.value
-	};
-	renderCard(valueCard);
-	turnOffButtom(popupAddSaveButton, validationConfig);
-	popupAddForm.reset();
-	closeModalWindow(popupAddElement);
+  evt.preventDefault()
+  const valueCard = {
+    name: popupAddFieldName.value,
+    link: popupAddFieldImage.value
+  };
+  renderCard(valueCard);
+  turnOffButtom(popupAddSaveButton, validationConfig);
+  popupAddForm.reset();
+  closeModalWindow(popupAddElement);
 }
 
 // ф-я выключающая кнопку
 const turnOffButtom = (buttonElement, validationConfig) => {
-	buttonElement.setAttribute("disabled", true);
-	buttonElement.classList.add(validationConfig.inactiveButtonClass);
+  buttonElement.setAttribute("disabled", true);
+  buttonElement.classList.add(validationConfig.inactiveButtonClass);
 }
 
 // ф-я открытия popup image
 const openModalImage = function(card) {
-	textPopup.textContent = card.name;
-	popupImage.src = card.link;
-	popupImage.alt = card.name;
-	openModalWindow(imagePopup);
+  textPopup.textContent = card.name;
+  popupImage.src = card.link;
+  popupImage.alt = card.name;
+  openModalWindow(imagePopup);
 }
 
 popupEditButtonElement.addEventListener('click', () => {
-	openEditModalWindow(popupEditElement);
+  openEditModalWindow(popupEditElement);
 });
 
 profileAddButtonElement.addEventListener('click', () => {
-	openModalWindow(popupAddElement);
+  openModalWindow(popupAddElement);
 });
 
 popupEditForm.addEventListener('submit', saveEditPopupValues);
@@ -112,30 +112,30 @@ popupAddForm.addEventListener('submit', addNewCard);
 
 // закрытие по нажатию на оверлей
 allPopups.forEach(popup => {
-	popup.addEventListener('mousedown', (evt) => {
-		if (evt.target === evt.currentTarget) {
-			closeModalWindow(popup);
-		}
-	})
+  popup.addEventListener('mousedown', (evt) => {
+    if (evt.target === evt.currentTarget) {
+      closeModalWindow(popup);
+    }
+  })
 })
 
 // добавление листенер на кнопки закрытия
 buttonCloseList.forEach(btn => {
-	const popup = btn.closest('.popup');
-	btn.addEventListener('click', () => closeModalWindow(popup))
+  const popup = btn.closest('.popup');
+  btn.addEventListener('click', () => closeModalWindow(popup))
 })
 
 // добавляем изначальные карточки на страничку
 initialCards.forEach(renderCard)
 
 const validationConfig = {
-	formSelector: '.popup__form',
-	inputSelector: '.popup__field',
-	submitButtonSelector: '.popup__save-button',
-	sectionSelector: '.popup__section',
-	errorInputSelector: '.popup__field-error',
-	errorInputActiveClass: 'popup__input-error_active',
-	inactiveButtonClass: 'popup__save-button_inactive'
+  formSelector: '.popup__form',
+  inputSelector: '.popup__field',
+  submitButtonSelector: '.popup__save-button',
+  sectionSelector: '.popup__section',
+  errorInputSelector: '.popup__field-error',
+  errorInputActiveClass: 'popup__input-error_active',
+  inactiveButtonClass: 'popup__save-button_inactive'
 }
 
 const addPopupValidator = new FormValidator(validationConfig, popupAddForm);
