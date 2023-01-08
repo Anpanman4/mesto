9a6d905c62515ab0dfd7d1029a8f487497bb8692
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-01-08 22:12:34 +0300

    feat: do some refactor

--- scripts/index.js
+++ scripts/index.js
@@ -18,10 +18,65 @@ import {
   elementContainer
 } from '../utils/constants.js'
 
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__field',
+  submitButtonSelector: '.popup__save-button',
+  sectionSelector: '.popup__section',
+  errorInputSelector: '.popup__field-error',
+  errorInputActiveClass: 'popup__input-error_active',
+  inactiveButtonClass: 'popup__save-button_inactive'
+}
+
 // код для работы Popup с Image
 const popupImageClass = new PopupWithImage('.popup_type_image');
 
 popupImageClass.setEventListeners();
+
+
+// код для работы валидации
+const addPopupValidator = new FormValidator(validationConfig, popupAddForm);
+const editPopupValidator = new FormValidator(validationConfig, popupEditForm);
+addPopupValidator.enableValidation();
+editPopupValidator.enableValidation();
+
+
+// UserInfo - код для управления информации о пользователе
+const userInfo = new UserInfo('.profile__name', '.profile__job')
+
+
+// PopupEdit - код для работы Popup редактирования пользователя
+const popupEditClass = new PopupWithForm({submitFunc: (evt, inputValue) => {
+  evt.preventDefault();
+
+  userInfo.setUserInfo(inputValue)
+
+  popupEditClass.close();
+}}, '.popup_type_edit')
+
+popupEditClass.setEventListeners();
+
+popupEditButtonElement.addEventListener('click', () => {
+  popupEditClass.open();
+});
+
+
+// PopupAdd - код для работы Popup добавления карточки
+const popupAddClass = new PopupWithForm({submitFunc: (evt, inputValue) => {
+  evt.preventDefault();
+
+  console.log(inputValue)
+  renderCard(inputValue);
+  turnOffButtom(popupAddSaveButton, validationConfig);
+  popupAddClass.close();
+}}, '.popup_type_add')
+
+popupAddClass.setEventListeners();
+
+profileAddButtonElement.addEventListener('click', () => {
+  popupAddClass.open();
+})
+
 
 // ф-я создания карточки с совсеми ивеннтами
 const makeCard = function(card) {
@@ -41,53 +96,3 @@ const turnOffButtom = (buttonElement, validationConfig) => {
   buttonElement.setAttribute("disabled", true);
   buttonElement.classList.add(validationConfig.inactiveButtonClass);
 }
-
-const validationConfig = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__field',
-  submitButtonSelector: '.popup__save-button',
-  sectionSelector: '.popup__section',
-  errorInputSelector: '.popup__field-error',
-  errorInputActiveClass: 'popup__input-error_active',
-  inactiveButtonClass: 'popup__save-button_inactive'
-}
-
-// код для работы валидации
-const addPopupValidator = new FormValidator(validationConfig, popupAddForm);
-const editPopupValidator = new FormValidator(validationConfig, popupEditForm);
-addPopupValidator.enableValidation();
-editPopupValidator.enableValidation();
-
-// UserInfo - код для управления информации о пользователе
-const userInfo = new UserInfo('.profile__name', '.profile__job')
-
-// PopupEdit - код для работы Popup редактирования пользователя
-const popupEditClass = new PopupWithForm({submitFunc: (evt, inputValue) => {
-  evt.preventDefault();
-
-  userInfo.setUserInfo(inputValue)
-
-  popupEditClass.close();
-}}, '.popup_type_edit')
-
-popupEditClass.setEventListeners();
-
-popupEditButtonElement.addEventListener('click', () => {
-  popupEditClass.open();
-});
-
-// PopupAdd - код для работы Popup добавления карточки
-const popupAddClass = new PopupWithForm({submitFunc: (evt, inputValue) => {
-  evt.preventDefault();
-
-  console.log(inputValue)
-  renderCard(inputValue);
-  turnOffButtom(popupAddSaveButton, validationConfig);
-  popupAddClass.close();
-}}, '.popup_type_add')
-
-popupAddClass.setEventListeners();
-
-profileAddButtonElement.addEventListener('click', () => {
-  popupAddClass.open();
-})
